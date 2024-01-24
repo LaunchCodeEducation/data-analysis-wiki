bd29a2a844828131f59602a39a40087f956736de
Sally Steuterman <sally.steuterman@gmail.com>
2024-01-24 12:09:10 -0600

    Updating build script

--- deploy-to-wiki.sh
+++ deploy-to-wiki.sh
@@ -1,16 +1,16 @@
 # Shell script that will deploy the master branch to the github
 # repo behind the intro wiki https://github.com/LaunchCodeEducation/data-analysis-wiki/wiki
 
-# WARNING: This script assumes you have added a remote named upstream that points to git@github.com:LaunchCodeEducation/data-analysis.wiki.git
-# If you haven't, please run this command: git remote add upstream git@github.com:LaunchCodeEducation/data-analysis.wiki.git
+# WARNING: This script assumes you have added a remote named upstream that points to git@github.com:LaunchCodeEducation/data-analysis-curriculum.wiki.git
+# If you haven't, please run this command: git remote add upstream git@github.com:LaunchCodeEducation/data-analysis-curriculum.wiki.git
 
 # WARNING: It's possible that pulling in main from origin or upstream could cause merge conflicts. You will need to resove those.
 
 
-echo "Updating with master then deploying"
-git checkout master
+echo "Updating with main then deploying"
+git checkout main
 git fetch origin
-git merge master --no-edit
+git merge main --no-edit
 git fetch upstream
-git merge upstream/master --no-edit
-git push upstream master
+git merge upstream/main --no-edit
+git push upstream main
